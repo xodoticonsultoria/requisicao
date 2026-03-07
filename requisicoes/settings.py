91c6f4ac5380bfeda40b86b6d5dd3885f21f67aa
DIOOWS <dioowdioowtrd@gmail.com>
2026-03-07 20:54:26 -0300

    fix allowed hosts render corrigido

--- requisicoes/settings.py
+++ requisicoes/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "unsafe-secret-key")
 DEBUG = os.environ.get("DEBUG", "0") == "1"
 
 ALLOWED_HOSTS = [
-    "https://requisicao-fvwx.onrender.com",
+    "requisicao-fvwx.onrender.com",
     "127.0.0.1",
     "localhost",
 ]
