c7ea455db80138416ad6db9c3bcb64bbd7cca22d
DIOOWS <dioowdioowtrd@gmail.com>
2026-03-17 14:59:12 -0300

    CORRIGINDO O ERRO DA RENATA FALA SERIO RENATA

--- requisicoes/settings.py
+++ requisicoes/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "unsafe-secret-key")
 DEBUG = os.environ.get("DEBUG", "0") == "1"
 
 ALLOWED_HOSTS = [
-    "requisicao-fvwx.onrender.com",
+    "requisicao-1aqm.onrender.com",
     "127.0.0.1",
     "localhost",
 ]
